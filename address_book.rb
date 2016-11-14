83de56b250e4f1d9a966de982b565fe0571b390a
SpencerHorn <spencer.horn@smashingboxes.com>
2016-11-14 15:44:06 -0500

    part seven

--- address_book.rb
+++ address_book.rb
@@ -7,6 +7,24 @@ class AddressBook
     @contacts = []
   end
 
+  def find_by_name(name)
+    results = []
+    search = name.downcase
+    contacts.each do |contact|
+      if contact.full_name.downcase.include?(search)
+        results.push(contact)
+      end
+    end
+    puts "Name search results (#{search})"
+    puts "-" * 50
+    results.each do |contact|
+      puts contact.to_s('full_name')
+      contact.print_phone_numbers
+      contact.print_addresses
+      puts "\n"
+    end
+  end
+
   def print_contact_list
     puts "Contact List"
     contacts.each do |contact|
@@ -25,5 +43,15 @@ spencer.add_phone_number("Home", "123-123-1234")
 spencer.add_phone_number("Work", "123-123-4321")
 spencer.add_address("Home", "2610 test dr", "", "Durham", "NC", "27701")
 
+andrea = Contact.new
+andrea.first_name = "Andrea"
+andrea.middle_name = "Louise-Myrick"
+andrea.last_name = "Horn"
+andrea.add_phone_number("Home", "123-123-1234")
+andrea.add_phone_number("Work", "123-123-4321")
+andrea.add_address("Home", "2610 test dr", "", "Durham", "NC", "27701")
+
 address_book.contacts.push(spencer)
-address_book.print_contact_list
+address_book.contacts.push(andrea)
+
+address_book.find_by_name('o')
